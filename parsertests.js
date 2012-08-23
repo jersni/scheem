522427b5748b391fa0a28b4d5345eb8d04d71fb0
Jerome <jersni@gmail.com>
2012-08-23 19:52:27 -0400

    added tests. added rules to ignore whitespace

---
+++ parsertests.js
@@ -0,0 +1,20 @@
+var PEG = require('pegjs');
+var assert = require('assert');
+var fs = require('fs'); // for loading files
+
+// Read file contents
+var data = fs.readFileSync('parser.peg', 'utf-8');
+// Show the PEG grammar file
+console.log(data);
+// Create my parser
+var parse = PEG.buildParser(data).parse;
+// Do a test
+assert.deepEqual( parse("atom"), "atom" );
+assert.deepEqual( parse("( )"), [] );
+assert.deepEqual( parse("(inneratom)"), ["inneratom"] );
+
+assert.deepEqual( parse("( a b c )"), ["a", "b", "c"] );
+assert.deepEqual( parse("(+ (+ 1 2) 2)"), ["+", ["+", "1", "2"], "2"] );
+assert.deepEqual( parse("(car\t(\nquote 1 2 3)\n)"), ["car", ["quote","1","2","3"]] );
+assert.deepEqual(parse("(   car ( cdr\t(\nquote   1 2 (+ 1 2)   3)   \n  ))"), 
+         ["car", ["cdr", ["quote", "1", "2", ["+", "1", "2"], "3"]]]);
