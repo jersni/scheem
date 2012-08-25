5ec67cc43165a6c8d776fce23df5e6d058453a6e
Jerome <jersni@gmail.com>
2012-08-24 22:18:47 -0400

    Added support for comments

--- parsertests.js
+++ parsertests.js
@@ -20,3 +20,7 @@ assert.deepEqual( parse("(   car ( cdr\t(\nquote   1 2 (+ 1 2)   3)   \n  ))"),
          ["car", ["cdr", ["quote", "1", "2", ["+", "1", "2"], "3"]]]);
 assert.deepEqual( parse("'a"), ["quote", "a"] );
 assert.deepEqual( parse("'(a b c)"), ["quote", ["a", "b", "c"]] );
+assert.deepEqual( parse(";; Comment\na"), "a" );
+
+var commentIn = ";; comment\n" + "(1 2 3);; comment after";
+assert.deepEqual( parse(commentIn), ["1", "2", "3"] );
